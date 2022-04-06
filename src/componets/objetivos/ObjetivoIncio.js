25396cd23a1cc792f272d850ab66a2c34a68c59a
cghiorg <cghiorg@gmail.com>
2022-04-06 11:23:39 -0300

    Cambios Gannt

--- src/componets/objetivos/ObjetivoIncio.js
+++ src/componets/objetivos/ObjetivoIncio.js
@@ -1,5 +1,5 @@
 
-import React from "react";
+import React, { useState } from "react";
 import MaterialTable from '@material-table/core';
 import Internacional from '../../service/Internacional';
 import { Card, CardContent, Typography } from '@mui/material';
@@ -15,9 +15,9 @@ import Avatar from '@mui/material/Avatar';
 import CalendarMonthIcon from '@mui/icons-material/CalendarViewMonth';
 import LinearProgress, { linearProgressClasses } from '@mui/material/LinearProgress';
 import { styled } from '@mui/material/styles';
-import { Chart } from "react-google-charts";
-import Diagram, { useSchema, createSchema } from 'beautiful-react-diagrams';
-import 'beautiful-react-diagrams/styles.css';
+import { Gantt, Task, EventOption, StylingOption, ViewMode, DisplayOption } from 'gantt-task-react';
+import "gantt-task-react/dist/index.css";
+
 
 const BorderLinearProgress = styled(LinearProgress)(({ theme }) => ({
     height: 10,
@@ -31,63 +31,38 @@ const BorderLinearProgress = styled(LinearProgress)(({ theme }) => ({
     },
 }));
 
-// export const options = {
-//     gantt: {
-//         criticalPathEnabled: false, // Critical path arrows will be the same as other arrows.
-//         arrow: {
-//             angle: 100,
-//             width: 5,
-//             color: "green",
-//             radius: 0,
-//         },
-//     },
-// };
-
-// const columns = [
-//     { type: "string", label: "Identificador" },
-//     { type: "string", label: "Nombre" },
-//     { type: "date", label: "Fecha Inicio" },
-//     { type: "date", label: "Fecha Finalizacion" },
-//     { type: "number", label: "Duracion" },
-//     { type: "number", label: "Porcentaje Completo" },
-//     { type: "string", label: "Dependencia" }
-// ];
-
-// let rows = datosTemas.map(function (te) {
-//     return [String(te.id), te.name, new Date(te.startdate), new Date(te.endadate), te.duration, te.percentcomplete, String(te.dependencies)];
-// });
-// export const data = [columns, ...rows];
+let rows = datosTemas.map(te => {
+    let datos = {
+        start: new Date(te.startdate),
+        end: new Date(te.endadate),
+        name: String(te.name),
+        id: String(te.id),
+        type: 'task',
+        progress: String(te.percentcomplete),
+        dependencies: [String(te.dependencies)],
+        styles: { progressColor: '#ffbb54', progressSelectedColor: '#ff9e0d' }
+    }
+    return datos
+});
 
 
 export default function ObjetivoIncio() {
-
-    const initialSchema = createSchema({
-        nodes: [
-            { id: 'node-0', content: 'Mis Objetivos', coordinates: [30, 110], },
-            { id: 'node-1', content: 'Objetivo A1', coordinates: [150, 60], },
-            { id: 'node-2', content: 'Inicio 29/05/2022', coordinates: [300, 60], },
-            { id: 'node-3', content: 'Fin 23/02/2023', coordinates: [700, 60], },
-            { id: 'node-4', content: 'Objetivo A2', coordinates: [150, 160], },
-            { id: 'node-5', content: 'Inicio 29/07/2022', coordinates: [300, 160], },
-            { id: 'node-6', content: 'Fin 23/10/2024', coordinates: [700, 160], },
-        ],
-        links: [
-            { input: 'node-0', output: 'node-1', alignment: 'right'},
-            { input: 'node-0', output: 'node-4', alignment: 'right'},
-            { input: 'node-2', output: 'node-3', label: '80%', alignment: 'right'},
-            { input: 'node-1', output: 'node-2', alignment: 'right'},
-            { input: 'node-4', output: 'node-5', alignment: 'right'},
-            { input: 'node-5', output: 'node-6',label: '80%', alignment: 'right'},
-        ]
-    });
-    
-    const [schema, { onChange }] = useSchema(initialSchema);
-
 
     const datoEstructura = datos.estructura;
     const datosDependecias = datos.dep;
     const datosObjetivos = datos.obj;
     const datosActividades = datos.act;
+    const [view, setView] = useState(ViewMode.Month);
+    const [isChecked, setIsChecked] = useState(false);
+
+    let columnWidth = 60;
+    // if (view === ViewMode.Month) {
+    //   columnWidth = 300;
+    // } else if (view === ViewMode.Week) {
+    //   columnWidth = 250;
+    // }
+    console.log(view)
+
 
     return (
         <Grid container spacing={2}>
@@ -211,38 +186,14 @@ export default function ObjetivoIncio() {
                 </Grid>
                 <Grid item xs={12} lg={12}>
                     <Paper sx={{ p: 2, display: 'flex', flexDirection: 'column' }} >
-                        {/* <MaterialTable
-                            columns={[
-                                { title: 'Nombre', field: 'nombre',
-                                cellStyle: (rowDataArray, rowData) => {
-                                    const paddingLeft = 15 * rowData.level;
-                                    return ({ paddingLeft: paddingLeft });
-                                }
-                            },
-                            {
-                                title: 'Expected Result',
-                                field: 'expectedResult',
-                                render: (rowData) => (
-                                    <BorderLinearProgress variant="determinate" value={rowData.prefer} />
-                                )
-                            }
-                            ]}
-                            title="Temas Estrategicos"
-                            data={datosTemas}
-                            collapseContent={true}
-                            localization={Internacional}
-                            parentChildData={(row, rows) => rows.find(a => a.id === row.parent)}
+                        <Gantt
+                            tasks={rows}
+                            ViewMode={view}
+                            locale='spa'
+                            listCellWidth={isChecked ? "155px" : ""}
+                            columnWidth={columnWidth}
+                            DisplayOption={true}
                         />
-                        <Chart
-                            chartType="Gantt"
-                            width="100%"
-                            height="50%"
-                            data={data}
-                            options={options}
-                        /> */}
-                        <div style={{ height: '22.5rem' }}>
-                            <Diagram schema={schema} onChange={onChange} />
-                        </div>
                     </Paper>
                 </Grid>
             </Grid>
